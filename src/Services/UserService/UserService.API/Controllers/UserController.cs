8522553dd6acbea893b562a447f8f581ceb95766
sumithkmohan199 <sumithkmohan@gmail.com>
2026-04-21 20:53:52 +0530

    Added extra spaces

--- src/Services/UserService/UserService.API/Controllers/UserController.cs
+++ src/Services/UserService/UserService.API/Controllers/UserController.cs
@@ -42,7 +42,7 @@ namespace UserService.API.Controllers
 
             var token = _tokenService.GetToken(user.UserId, user.Role);
 
-            return Ok( new { Token = token });
+            return Ok( new {  Token = token });
         }
 
         // GET api/<UserController>/5
